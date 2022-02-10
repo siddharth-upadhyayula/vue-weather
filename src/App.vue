ac09524c46205001eb630140d8ab1d13f5c2450a
sid_upadhyayula <s.upadhyayula1@ufl.edu>
2022-02-10 18:44:48 -0500

    Added Queries

--- src/App.vue
+++ src/App.vue
@@ -7,14 +7,15 @@
         class="search-bar" 
         placeholder="Search..."
         v-model="query"
+        @keypress="fetchWeather"
         
         />
         {{  query  }}
       </div>
 
-      <div class="weather-wrap">
+      <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
         <div class="location-box"> 
-          <div class="location">Northampton, UK </div>
+          <div class="location">{{ weather.name }} </div>
           <div class="date">Sunday January 30 2022</div>
         </div>
       </div>
@@ -40,7 +41,19 @@ export default {
       weather:{}
   }
 },
-
+  methods: {
+    fetchWeather (e) {
+      if (e.key == "Enter") {
+        fetch('${this.api_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}')
+          .then(res => {
+            return res.json();
+          }).then(this.setResults);
+      }
+    },
+    setResults (results) {
+      this.weather = results;
+    }
+  }
 }
 
 </script>
